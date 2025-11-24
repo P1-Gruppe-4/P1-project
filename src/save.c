2396a32d7d700ca1d8871b748c0434692bacea04
oscar <oeichm25@student.aau.dk>
2025-11-24 15:38:59 +0100

    file support can now read from and write to file

--- src/save.c
+++ src/save.c
@@ -3,21 +3,10 @@
 #include <stdlib.h>
 #include <assert.h>
 
-/*
-void auto_save(lot** parkinglot, int length, int width) {
-    FILE* autosave = fopen("autosave.txt", "w");
-    assert(autosave != NULL);
-
-    for (int i = 0; i < length; i++) {
-        fwrite(parkinglot[i], sizeof(lot), (size_t)width, autosave);
-    }
-    printf("Autosave completed\n");
-}
-*/
-
 void auto_save (lot** pointer, int length, int width)
 {
     FILE* auto_save = fopen("autosave.txt", "w");
+    fprintf(auto_save, "%d %d-", length, width); //burde virke da de første to værdier bliver læst i starten dvs. de er ikke tilgængelige for at blive indlæst senere igen
     for(int i=0; i<length; i++)
     {
         for(int j=0; j<width; j++)
@@ -33,16 +22,17 @@ void auto_save (lot** pointer, int length, int width)
 }
 
 lot** load_save(FILE* savefile) {
-    lot **parkingLot;
-    while (feof(savefile) <= 0) {
-        int counter_1 = 0, counter_2 = 0;
+    int length, width;
+    fscanf(savefile, "%d %d", &length, &width);
 
-        if (getc(savefile) == '\n') {
-            counter_1++;
+    lot **parkingLot = ArrayAlloc(length, width);
+    for(int i=0; i<length; i++) {
+        for(int j=0; j<width; j++) {
+            fscanf(savefile, "%d %d-", &parkingLot[i][j].status, &parkingLot[i][j].handicap);
         }
-        fscanf(savefile, "%[^-]d %[^-]d", parkingLot[counter_1][counter_2].status, parkingLot[counter_1][counter_2].handicap);
-        counter_2++;
     }
+
     fclose(savefile);
+    ArrayPrint(parkingLot, length, width);
     return parkingLot;
 }
