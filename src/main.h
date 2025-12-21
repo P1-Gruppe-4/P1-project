72d89c237fea271907c442a06487ca46019c3e6b
john <bzimm25@student.aau.dk>
2025-12-21 11:29:45 +0100

    main.h was stupid. Removed test.txt

--- src/main.h
+++ src/main.h
@@ -1,19 +1,7 @@
-//
-// Created by john on 21/11/2025.
-//
-
 #ifndef P1_GRUPPE4_MAIN_H
 #define P1_GRUPPE4_MAIN_H
 
 #include <stdio.h>
 #include "menu.h"
-
-/**
- * @brief Main function that calls main_menu
- *
- * @return 0 when program is done
- */
-int main();
-
 
 #endif //P1_GRUPPE4_MAIN_H
